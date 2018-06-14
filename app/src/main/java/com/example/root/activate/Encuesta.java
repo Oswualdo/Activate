41b720a1da1721ac939e5a16d5beb3d06f627acf
Ricardo BJ <benitezjimenezricardo@gmail.com>
2018-06-13 20:46:49 -0500

    Envia los datos de Perfil y Primera Encuesta al Servidor
    Aun los datos se encuentran en texto se necesita se pase a indice de opcion para sea mas facil de tratar a la hora de la visualizacion

--- app/src/main/java/com/example/root/activate/Encuesta.java
+++ app/src/main/java/com/example/root/activate/Encuesta.java
@@ -12,9 +12,33 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Encuesta extends AppCompatActivity {
+    /*Para el servidor*/
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public static final String NICKNAME_KEY="nickname";
+    public static final String ID_USUARIO_KEY="id_usuario";
+    public static final String COORDINACION_KEY="id_coordinacion";
+    public static final String LAST_UPDATE_KEY="Last_Update";
+    public static final String ROL_KEY="id_rol";
+    public static final String SEXO_KEY="Sexo";
+    public static final String EDAD_KEY="Edad";
+    public static final String USERS_REF="Usuarios";
+
+    public static final String ENCUESTA_REF="Encuesta";
+    public static final String ALIMENTACION_KEY="Alimentacion";
+    public static final String PESO_KEY="Peso";
+    public static final String ALTURA_KEY="Altura";
+    public static final String EJERCICIO_KEY="Ejercicio";
+    public static final String FUMAR_KEY="Fumar";
+    public static final String ALCOHOL_KEY="Alcohol";
+    public static final String HORAS_KEY="Horas";
 
     MaterialBetterSpinner alimentacion,ejercicio,fumar,tomar,estancia;
 
@@ -89,6 +113,31 @@ public class Encuesta extends AppCompatActivity {
                     //AQUI YA PUEDES MANDAR LOS DATOS QUE SE OBTIENEN DE LA APP
                     //A LA BASE DE DATOS
                     //Los datos son: id,NickName, Edad, Genero, Coordinacion, Rol, comida,deport,fum,tom,estanc,peso,altura
+
+                    /*para el servidor*/
+                    Map<String,Object> dataOfUser = new HashMap<String, Object>();
+                    dataOfUser.put(NICKNAME_KEY,NickName);
+                    dataOfUser.put(ID_USUARIO_KEY,id_user);
+                    dataOfUser.put(COORDINACION_KEY,Coordinacion);
+                    dataOfUser.put(ROL_KEY,Rol);
+                    dataOfUser.put(SEXO_KEY,Genero);
+                    dataOfUser.put(EDAD_KEY,Edad);
+                    //esto lo estoy probando como opcion
+                    long d = System.currentTimeMillis()/1000;
+                    dataOfUser.put(LAST_UPDATE_KEY,d);
+
+                    db.collection(USERS_REF).document(id_user).set(dataOfUser);
+
+                    Map<String,Object> encuestaToSend = new HashMap<String, Object>();
+                    encuestaToSend.put(ALIMENTACION_KEY,comida);
+                    encuestaToSend.put(PESO_KEY,peso);
+                    encuestaToSend.put(ALTURA_KEY,altura);
+                    encuestaToSend.put(EJERCICIO_KEY,deport);
+                    encuestaToSend.put(FUMAR_KEY,fum);
+                    encuestaToSend.put(ALCOHOL_KEY,tom);
+                    encuestaToSend.put(HORAS_KEY,estanc);
+
+                    db.collection(USERS_REF).document(id_user).collection(ENCUESTA_REF).document("1").set(encuestaToSend);
 
                     //para despues pasar a la siguiente pantalla
                     Intent intent = new Intent(Encuesta.this, MainActivity.class);
