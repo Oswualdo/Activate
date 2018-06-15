30fac987b6a1da9cfaeb860288bfaf9b781b9b0e
kenpa-1 <40267493+kenpa-1@users.noreply.github.com>
2018-06-15 16:19:34 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/root/activate/MainActivity.java
+++ app/src/main/java/com/example/root/activate/MainActivity.java
@@ -2,12 +2,51 @@ package com.example.root.activate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+//implements SensorEventListener 
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private TextView count;
+    boolean activityRunning;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Selecciona lugar donde se mostraran los datos
+        count = (TextView) findViewById(R.id.edtpasos);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        activityRunning = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Contador no disponible", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        activityRunning = false;
+        // En caso de que la pausen, se supone esto no va a pasar
+//        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        //Se actualiza el valor del contador
+        if (activityRunning) {
+            count.setText(String.valueOf(event.values[0]));
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 }
